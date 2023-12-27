81cd3e91a5950a06d7cd6a4d6b22f85e05329178
Tadesse <tadess1127@gmail.com>
2023-12-27 12:13:20 +0300

    view is  modified for authentication purpse

--- account/views.py
+++ account/views.py
@@ -34,18 +34,21 @@ def loginPage(request):
             return render(request, 'account/login.html', context)
 
 def registerPage(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
+    if request.user.is_authenticated:
+         return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
 
-            return redirect('login')
-        
-    context = {'form':form}
-    return render(request, 'account/register.html', context)
+                return redirect('login')
+            
+        context = {'form':form}
+        return render(request, 'account/register.html', context)
 
 def logoutUser(request):
 	logout(request)
